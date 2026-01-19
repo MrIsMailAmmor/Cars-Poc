7a37d3eded1cd37c1ee09d16e222e99e755e9282
ismail Ammor <ammor58@gmail.com>
2026-01-19 17:45:59 +0100

    revert

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-
-  nitro: {
-    // This tells Nitro to include the generated Prisma folder in the final build
-    moduleSideEffects: ["@prisma/client"],
-    externals: {
-      trace: true,
-      include: [".prisma"], // This is where the actual binaries and code live
-    },
+  build: {
+    transpile: ["@prisma/client"],
   },
 });
